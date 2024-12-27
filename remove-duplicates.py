c2b4bfaa3420a16930825bbc7ec3f235d2fbc8b4
hannan70 <hannan728070@gmail.com>
2024-12-27 12:25:07 +0600

    upload

--- remove-duplicates.py
+++ remove-duplicates.py
@@ -6,8 +6,23 @@ def remove_duplicate(nums):
         if nums[j] != nums[j - 1]:
             nums[i] = nums[j]
             i += 1
+            print(nums[i])
 
     return i
 
 nums = [0,0,1,1,1,2,2,3,3,4]
-print(remove_duplicate(nums))
+print("item => ", remove_duplicate(nums))
+
+
+
+# def remove_duplicate(nums):
+#     i = 1
+#     for j in range(1, len(nums)):
+#         if nums[j] != nums[j - 1]:
+#             nums[i] = nums[j]
+#             i += 1
+#
+#     return i
+#
+# nums = [0,0,1,1,1,2,2,3,3,4]
+# print(remove_duplicate(nums))
